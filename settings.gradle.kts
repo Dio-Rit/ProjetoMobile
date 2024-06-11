104c66cd2c6b3fe36a8af1bbd8f8afdefa75b269
Dio-Rit <dionatan.ritter@universo.univates.br>
2024-06-11 20:59:01 -0300

    2 commit 25min de video

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -12,12 +12,13 @@ pluginManagement {
     }
 }
 dependencyResolutionManagement {
+    @Suppress("UnstableApiUsage")
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    @Suppress("UnstableApiUsage")
     repositories {
         google()
         mavenCentral()
     }
 }
-
 rootProject.name = "ProjetoMobile"
 include(":app")
